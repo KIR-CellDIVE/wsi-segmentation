2684e287e7299f15f86518bbddb9c99e4162809d
Jonas Mackerodt <jonas.mackerodt@kennedy.ox.ac.uk>
2024-10-29 14:02:11 +0000

    removed unused functions and stray code

--- src/wsi_segmentation/segmentation_utils.py
+++ src/wsi_segmentation/segmentation_utils.py
@@ -38,95 +38,6 @@ def find_optimal_tile_size(x_dim, y_dim, overlap, max_tile_dim = 10000, max_tile
 
     return(int(tile_size_row), int(tile_size_col))
 
-def _xy_ratio(col_tile_size, row_tile_size):
-        if 0 in [col_tile_size, row_tile_size]:
-            return True
-        return(2.0 >= col_tile_size/row_tile_size >= 0.5)
-
-def _xy_ratios(img_col_dim, img_row_dim, col_tile_size, row_tile_size, n, overlap):
-    last_col_tile = _last_tile_dim(img_col_dim, col_tile_size, n, overlap)
-    last_row_tile = _last_tile_dim(img_row_dim, row_tile_size, n, overlap)
-    return(_xy_ratio(col_tile_size, row_tile_size) and _xy_ratio(last_col_tile, row_tile_size) and _xy_ratio(col_tile_size, last_row_tile) and _xy_ratio(last_col_tile, last_row_tile))
-
-def _max_tile_size(col_dim, row_dim, ref=pow(10000,2)):
-    return(col_dim*row_dim < ref)
-
-def _min_tile_size(col_dim, row_dim, ref=pow(500,2)):
-    return(col_dim*row_dim > ref)
-
-def _min_tile_size_cond(img_col_dim, img_row_dim, col_tile_size, row_tile_size, n, overlap, ref=pow(500,2)):
-    last_col_tile = _last_tile_dim(img_col_dim, col_tile_size, n, overlap)
-    last_row_tile = _last_tile_dim(img_row_dim, row_tile_size, n, overlap)
-
-    return(_min_tile_size(last_col_tile, last_row_tile, ref=ref))
-
-def _smallest_tile_dim(dim_size,n,overlap):
-    return(ceil(dim_size/n + overlap - overlap/n))
-
-def _smallest_tile_dim_const(smallest_tile_dim, factor=0.5):
-    return(ceil(factor*smallest_tile_dim))
-
-def _smallest_tile_dim_comb(dim_size,n,overlap,factor=0.5):
-    smallest_tile_dim = _smallest_tile_dim(dim_size,n,overlap)
-    smallest_tile_oppdim = _smallest_tile_dim_const(smallest_tile_dim, factor=factor)
-    return(smallest_tile_dim,smallest_tile_oppdim)
-
-def _largest_tile_tile_dim(side,n, overlap):
-    return(((3-n) * overlap - side)/(1-n))
-
-def _largest_tile_dim_const(largest_tile_dim, factor=2):
-    return(factor*largest_tile_dim)
-
-def _sum_tiles(tile_dim_size, n, overlap):
-    return(n*(tile_dim_size-overlap)+overlap)
-
-def _overhang(dim_size, tile_dim_size, n, overlap):
-    return(_sum_tiles(tile_dim_size, n, overlap) - dim_size)
-
-def _last_tile_dim(dim_size, tile_dim_size, n, overlap):
-    return(tile_dim_size - _overhang(dim_size, tile_dim_size, n, overlap))
-
-def combined_constraints(img_col_dim, img_row_dim, n, overlap, max_tile_area=pow(10000,2), min_tile_area=pow(500,2)):
-    # calculate if given n and overlap o what the smallest ci and ri is
-    sci = _smallest_tile_dim(img_col_dim, n, overlap)
-    sri = _smallest_tile_dim(img_row_dim, n, overlap)
-
-    if (_max_tile_size(sci, sri, ref=max_tile_area) == True):
-        if _xy_ratios(img_col_dim, img_row_dim, sci, sri, n, overlap) and _min_tile_size_cond(img_col_dim, img_row_dim, sci, sri, n, overlap, ref=min_tile_area):
-            return({'col_tile_size' : sci, 'row_tile_size' : sri, 'n_tiles' : n, 'overlap' : overlap})
-        else:
-            smallest_row_given_sci = _smallest_tile_dim_const(sci)
-            smallest_col_given_sri = _smallest_tile_dim_const(sri)
-            
-            if sci <= smallest_col_given_sri:
-                if _xy_ratios(img_col_dim, img_row_dim, smallest_col_given_sri, sri, n, overlap) and _min_tile_size_cond(img_col_dim, img_row_dim, smallest_col_given_sri, sri, n, overlap, ref=min_tile_area):
-                    return({'col_tile_size' : smallest_col_given_sri, 'row_tile_size' : sri, 'n_tiles' : n, 'overlap' : overlap})
-            elif sri <= smallest_row_given_sci:
-                if _xy_ratios(img_col_dim, img_row_dim, sci, smallest_row_given_sci, n, overlap) and _min_tile_size_cond(img_col_dim, img_row_dim, sci, smallest_row_given_sci, n, overlap, ref=min_tile_area):
-                    return({'col_tile_size' : sci, 'row_tile_size' : smallest_row_given_sci, 'n_tiles' : n, 'overlap' : overlap})            
-        return(None)
-    
-    else:
-        return(None) 
-
-    
-
-def tile_sizer(img_col_dim, img_row_dim, overlap, max_tile_area = pow(10000,2), min_tile_area = pow(500,2), n_tiles = range(2,11)):
-    res = None
-    
-    if _max_tile_size(img_col_dim,img_row_dim, ref=max_tile_area) and _xy_ratio(img_col_dim, img_row_dim):
-        res = {'col_tile_size' : img_col_dim, 'row_tile_size' : img_row_dim, 'n_tiles' : 1, 'overlap' : 0}
-    else:
-        for n in n_tiles:
-            res = combined_constraints(img_col_dim, img_row_dim, n, overlap, max_tile_area=max_tile_area, min_tile_area=min_tile_area)
-            if res != None:
-                break
-    
-    if res == None:
-         raise ValueError(f"No appropriate tile size for image of size {img_col_dim} x {img_row_dim} and overlap {overlap} could be automatically determined. Consider defining your own tile sizes via `tile_size_row` and `tile_size_col` arguments.")
-    else:
-        return(res)
-    
 
 def remove_boundary_mask(arr, boundary, boundary_sides, dummy_var):
     boundary_ids = list()
@@ -154,7 +65,6 @@ def remove_boundary_mask(arr, boundary, boundary_sides, dummy_var):
     boundary_ids = np.unique(boundary_ids)
     boundary_ids= [i for i in boundary_ids if i != 0]
     cleaned_arr = np.where(np.isin(arr, boundary_ids), dummy_var, arr)
-    ## replace zeros in boundary wit -99
 
     return(cleaned_arr)
     
@@ -190,8 +100,6 @@ def tiled_segmentation_overlap(img, start_row, start_col, stop_row, stop_col, st
                         
             boundaries = determine_boundaries(img, r0,r1,c0,c1)
             
-            # if np.max(img[:,:,:,:]) < background_threshold:
-            #     tmp_segmentation = np.zeros_like(mask_array, dtype=int)[:, r0:r1, c0:c1,:] ##  remove this step????
             if (np.max(img[:, r0:r1, c0:c1,:]) >= background_threshold) and (np.unique(img[:, r0:r1, c0:c1,0]).shape[0] > 1) and (np.unique(img[:, r0:r1, c0:c1,1]).shape[0] > 1) :
                 tmp_segmentation = app.predict(img[:, r0:r1, c0:c1,:], compartment=compartment, postprocess_kwargs_whole_cell=postprocess_kwargs_whole_cell, postprocess_kwargs_nuclear=postprocess_kwargs_nuclear)
 
@@ -200,13 +108,13 @@ def tiled_segmentation_overlap(img, start_row, start_col, stop_row, stop_col, st
 
                     tmp_segmentation[0,:,:,j] = make_cell_mask_unique(tmp_segmentation[0,:,:,j], dummy_var, max_current_cell_id[j])
                     max_current_cell_id[j] = np.maximum(0,np.max(tmp_segmentation[0,:,:,j]))
-            # for j in range(tmp_segmentation.shape[3]):  ##  move that in       
-            #     ### remove overlapping ids
+
                     insert_mask = np.isin(mask_array[0, r0:r1, c0:c1, j], [dummy_var, 0])
                     mask_array[0, r0:r1, c0:c1, j][insert_mask] = tmp_segmentation[0,:,:,j][insert_mask]
 
     gc.collect()
     return(mask_array)
+
 
 def getval_array(d):
     # based on https://stackoverflow.com/a/46870227
@@ -258,7 +166,6 @@ def predict_tiled(img, tile_size_row=None, tile_size_col=None, dummy_var=-99, ov
         fov = img[[fov_idx], ...]
         overlap = overlap if infer_gaps == True else 0
         if (tile_size_row) == None or (tile_size_col == None):
-            # tile = tile_sizer(fov.shape[2], fov.shape[1], overlap) if max_tile_area == None else tile_sizer(fov.shape[2], fov.shape[1], overlap, max_tile_area=max_tile_area)
             tile = find_optimal_tile_size(fov.shape[1], fov.shape[2], overlap, max_tile_dim = max_tile_dim, max_tile_area = max_tile_area)
             step_size_row = tile[0]
             step_size_col = tile[1]
